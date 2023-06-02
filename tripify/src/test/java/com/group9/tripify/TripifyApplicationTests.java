cd9cd89afe3683138bdb89812dd0f92d96d07ec6
Jaskaran Singh <js356337@dal.ca>
2023-06-02 10:37:10 -0300

    first commit

---
+++ tripify/src/test/java/com/group9/tripify/TripifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group9.tripify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
