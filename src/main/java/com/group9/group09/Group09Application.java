9bb0ec9a50e5d572074ea70451a8e86368e95556
Jaskaran762 <singhjaskaran762@gmail.com>
2023-06-06 16:22:08 -0300

    project setup

---
+++ src/main/java/com/group9/group09/Group09Application.java
@@ -0,0 +1,14 @@
+package com.group9.group09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group09Application.class, args);
+		System.out.println("hi");
+	}
+
+}
