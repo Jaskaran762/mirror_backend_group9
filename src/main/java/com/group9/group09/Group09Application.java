128299cf82869f7ed303bb7a027cdc99c32551b9
Jaskaran762 <singhjaskaran762@gmail.com>
2023-06-09 02:11:49 -0300

    pipeline test

--- src/main/java/com/group9/group09/Group09Application.java
+++ src/main/java/com/group9/group09/Group09Application.java
@@ -8,7 +8,7 @@ public class Group09Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Group09Application.class, args);
-		System.out.println("Test Script");
+		System.out.println("Test Script 33");
 	}
 
 }
