0a0e529b1ef132df0b91cd0b68d25648b908b97b
Jaskaran762 <singhjaskaran762@gmail.com>
2023-06-16 02:30:28 -0300

    TDD for userManagement module

--- src/main/java/com/group9/group09/controller/TripifyTestController.java
+++ src/main/java/com/group9/group09/controller/TripifyTestController.java
@@ -1,6 +1,7 @@
 package com.group9.group09.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +11,5 @@ public class TripifyTestController {
     public String testTripify(){
         return "hello world, is it working still";
     }
+
 }
