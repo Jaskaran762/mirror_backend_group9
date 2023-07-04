9884d5664c6820b64ae93699e696fa34eccd351a
Jaskaran762 <singhjaskaran762@gmail.com>
2023-07-04 15:08:08 -0300

    bug fix

---
+++ src/main/java/com/group9/group09/DTO/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.group9.group09.DTO;
+
+public class ErrorResponse {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
