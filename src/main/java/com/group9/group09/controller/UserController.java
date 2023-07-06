e9a82dae962ddd35d6c294944643bee3672c9bfd
Jaskaran762 <singhjaskaran762@gmail.com>
2023-07-06 15:19:56 -0300

    merge check

--- src/main/java/com/group9/group09/controller/UserController.java
+++ src/main/java/com/group9/group09/controller/UserController.java
@@ -10,6 +10,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * sddf
+ *
+ */
 @RestController
 @RequestMapping(path = "/auth")
 @CrossOrigin
