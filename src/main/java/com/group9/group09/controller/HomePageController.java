4242f18539694db2cfd5b53215e354db9f5342e8
Jaskaran762 <singhjaskaran762@gmail.com>
2023-07-15 05:25:40 -0300

    token issue fix

--- src/main/java/com/group9/group09/controller/HomePageController.java
+++ src/main/java/com/group9/group09/controller/HomePageController.java
@@ -5,6 +5,7 @@ import com.group9.group09.DTO.ChoiceResponseDTO;
 import com.group9.group09.DTO.ErrorResponse;
 import com.group9.group09.DTO.LocationRequestDTO;
 import com.group9.group09.service.interfaces.HomePageService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,12 +20,14 @@ public class HomePageController {
     private HomePageService homeService;
 
     @PostMapping(path = "/choice")
-    public ResponseEntity<?> choiceSelector(@RequestBody ChoiceRequestDTO choice) {
+    public ResponseEntity<?> choiceSelector(@RequestBody ChoiceRequestDTO choice, HttpServletRequest request) {
 
         try {
+                choice.setToken(request.getHeader("Authorization").replace("Bearer ",""));
                 ChoiceResponseDTO choiceResponseDTO =  homeService.choiceSelectorService(choice);
                 return new ResponseEntity<>(choiceResponseDTO, HttpStatus.OK);
         }catch (Exception e){
+            System.out.println(e);
             ErrorResponse response = new ErrorResponse();
             response.setMessage("Choice selector api failed");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
