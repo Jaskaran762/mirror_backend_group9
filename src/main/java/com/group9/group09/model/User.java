c50e70fa3c3549b09d78531e86467fdcdc81926c
boza <bh327148@dal.ca>
2023-07-18 01:43:07 -0300

    Adding Documentation comments for model package

--- src/main/java/com/group9/group09/model/User.java
+++ src/main/java/com/group9/group09/model/User.java
@@ -16,34 +16,74 @@ public class User implements UserDetails {
 	private String phone;
 	private Integer homeCountry;
 
+	/**
+	 * Get the ID of the user's home country.
+	 *
+	 * @return the homeCountry
+	 */
 	public Integer getHomeCountry() {
 		return homeCountry;
 	}
 
+	/**
+	 * Set the ID of the user's home country.
+	 *
+	 * @param homeCountry the homeCountry to set
+	 */
 	public void setHomeCountry(Integer homeCountry) {
 		this.homeCountry = homeCountry;
 	}
 
+	/**
+	 * Get the name of the user.
+	 *
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Set the name of the user.
+	 *
+	 * @param name the name to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Get the ID of the user.
+	 *
+	 * @return the userId
+	 */
 	public String getUserId() {
 		return userId;
 	}
 
+	/**
+	 * Set the ID of the user.
+	 *
+	 * @param userId the userId to set
+	 */
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
 
+	/**
+	 * Get the email of the user.
+	 *
+	 * @return the email
+	 */
 	public String getEmail() {
 		return email;
 	}
 
+	/**
+	 * Set the email of the user.
+	 *
+	 * @param email the email to set
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
@@ -53,6 +93,11 @@ public class User implements UserDetails {
 		return null;
 	}
 
+	/**
+	 * Get the password of the user.
+	 *
+	 * @return the password
+	 */
 	public String getPassword() {
 		return password;
 	}
@@ -82,6 +127,11 @@ public class User implements UserDetails {
 		return true;
 	}
 
+	/**
+	 * Set the password of the user.
+	 *
+	 * @param password the password to set
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
@@ -108,12 +158,22 @@ public class User implements UserDetails {
 	public String toString() {
 		return "User [name=" + name + ", username=" + userId + ", email=" + email + ", password=" + password
 				+ "]";
-	}  
+	}
 
+	/**
+	 * Get the phone number of the user.
+	 *
+	 * @return the phone
+	 */
 	public String getPhone() {
 		return phone;
 	}
 
+	/**
+	 * Set the phone number of the user.
+	 *
+	 * @param phone the phone to set
+	 */
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
--- src/main/java/com/group9/group09/model/wishList.java
+++ src/main/java/com/group9/group09/model/wishList.java
@@ -1,43 +1,79 @@
 package com.group9.group09.model;
 
-import com.group9.group09.repository.PlaceRepositoryImp;
-import org.springframework.security.access.prepost.PrePostInvocationAttributeFactory;
-
 public class wishList {
     private Integer wishListID;
     private Integer userID;
     private Integer placeID;
     private Integer activityID;
 
-
+    /**
+     * Get the ID of the wishlist.
+     *
+     * @return the wishListID
+     */
     public Integer getWishListID() {
         return wishListID;
     }
 
+    /**
+     * Set the ID of the wishlist.
+     *
+     * @param wishListID the wishListID to set
+     */
     public void setWishListID(Integer wishListID) {
         this.wishListID = wishListID;
     }
 
+    /**
+     * Get the ID of the user.
+     *
+     * @return the userID
+     */
     public Integer getUserID() {
         return userID;
     }
 
+    /**
+     * Set the ID of the user.
+     *
+     * @param userID the userID to set
+     */
     public void setUserID(Integer userID) {
         this.userID = userID;
     }
 
+    /**
+     * Get the ID of the place.
+     *
+     * @return the placeID
+     */
     public Integer getPlaceID() {
         return placeID;
     }
 
+    /**
+     * Set the ID of the place.
+     *
+     * @param placeID the placeID to set
+     */
     public void setPlaceID(Integer placeID) {
         this.placeID = placeID;
     }
 
+    /**
+     * Get the ID of the activity.
+     *
+     * @return the activityID
+     */
     public Integer getActivityID() {
         return activityID;
     }
 
+    /**
+     * Set the ID of the activity.
+     *
+     * @param activityID the activityID to set
+     */
     public void setActivityID(Integer activityID) {
         this.activityID = activityID;
     }
