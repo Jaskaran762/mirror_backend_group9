a673027d5c39668a644efaaeebd32f325a57c88e
Aharnish Maheshbhai Solanki <ah910744@dal.ca>
2023-07-20 15:51:33 -0300

    packages imported for logger

--- src/main/java/com/group9/group09/service/HomePageServiceImpl.java
+++ src/main/java/com/group9/group09/service/HomePageServiceImpl.java
@@ -7,7 +7,8 @@ import com.group9.group09.repository.interfaces.*;
 import com.group9.group09.service.interfaces.HomePageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
