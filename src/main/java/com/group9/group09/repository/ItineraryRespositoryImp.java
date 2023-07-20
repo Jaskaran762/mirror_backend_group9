ec37eb8dbdd7e1457967cf8ceee4efed9388d10d
Riya <ry470536@dal.ca>
2023-07-20 16:53:11 -0300

    Logger added to ItineraryRepositoryIMP

--- src/main/java/com/group9/group09/repository/ItineraryRespositoryImp.java
+++ src/main/java/com/group9/group09/repository/ItineraryRespositoryImp.java
@@ -5,6 +5,8 @@ import com.group9.group09.model.Itinerary;
 import com.group9.group09.repository.interfaces.ItineraryRepository;
 import com.group9.group09.repository.rowmapper.ItemstoCarryRowMapper;
 import com.group9.group09.repository.rowmapper.ItineraryRowMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -14,6 +16,7 @@ import java.util.List;
 public class ItineraryRespositoryImp implements ItineraryRepository {
     private final JdbcTemplate jdbcTemplate;
 
+    private static Logger logger = LoggerFactory.getLogger(ItemsRepositoryImp.class);
     public ItineraryRespositoryImp(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
@@ -22,9 +25,11 @@ public class ItineraryRespositoryImp implements ItineraryRepository {
     public List<Itinerary> getItineraryList(Integer userid) {
 
         try {
+            logger.info("Info Message: ");
             String getItinerarybyUserIDQuery = "SELECT * FROM Itinerary where user_id=?";
             return jdbcTemplate.query(getItinerarybyUserIDQuery, new ItineraryRowMapper(), userid);
         } catch (Exception e) {
+            logger.error("Error Message: ");
             throw new RuntimeException(e.getMessage());
         }
 
