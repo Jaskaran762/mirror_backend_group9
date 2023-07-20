defe5df10f6869cf5ec73523689dddb99d8dad73
Riya <ry470536@dal.ca>
2023-07-20 16:47:51 -0300

    Logger added to ItemsRepositoryIMP

--- src/main/java/com/group9/group09/repository/ItemsRepositoryImp.java
+++ src/main/java/com/group9/group09/repository/ItemsRepositoryImp.java
@@ -4,6 +4,8 @@ import com.group9.group09.model.ItemstoCarry;
 import com.group9.group09.repository.interfaces.ItemsRepository;
 import com.group9.group09.repository.rowmapper.ActivityRowMapper;
 import com.group9.group09.repository.rowmapper.ItemstoCarryRowMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -14,6 +16,8 @@ public class ItemsRepositoryImp implements ItemsRepository{
 
     private final JdbcTemplate jdbcTemplate;
 
+    private static Logger logger = LoggerFactory.getLogger(ItemsRepositoryImp.class);
+
     public ItemsRepositoryImp(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
@@ -22,9 +26,11 @@ public class ItemsRepositoryImp implements ItemsRepository{
     public List<ItemstoCarry> getAllItems() {
 
         try {
+            logger.info("Info Message: ");
             String getAllItemstoCarryQuery = "SELECT * FROM Itemstocarry";
             return jdbcTemplate.query(getAllItemstoCarryQuery, new ItemstoCarryRowMapper(), null);
         } catch (Exception e) {
+            logger.error("Error Message: ");
             throw new RuntimeException(e.getMessage());
         }
     }
