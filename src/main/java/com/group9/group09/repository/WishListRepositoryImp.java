7b7b2065fbd82f3a2c26890d6ae274531dd75df8
Riya <ry470536@dal.ca>
2023-07-20 17:04:42 -0300

    Logger added to WishListRepositoryIMP

--- src/main/java/com/group9/group09/repository/WishListRepositoryImp.java
+++ src/main/java/com/group9/group09/repository/WishListRepositoryImp.java
@@ -3,6 +3,8 @@ package com.group9.group09.repository;
 import com.group9.group09.model.wishList;
 import com.group9.group09.repository.interfaces.WishlistRepository;
 import com.group9.group09.repository.rowmapper.WishListRowMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +15,7 @@ public class WishListRepositoryImp implements WishlistRepository {
 
     private final JdbcTemplate jdbcTemplate;
 
+    private static Logger logger = LoggerFactory.getLogger(WishListRepositoryImp.class);
     public WishListRepositoryImp(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
@@ -21,10 +24,12 @@ public class WishListRepositoryImp implements WishlistRepository {
     public List<wishList> getWishListbyUserID(Integer userID) {
 
         try{
-                String getWishListbyUserIDQuery = "Select * from Wishlist where user_id=?";
-                return jdbcTemplate.query(getWishListbyUserIDQuery,new WishListRowMapper(),userID);
+            logger.info("Info Message: ");
+            String getWishListbyUserIDQuery = "Select * from Wishlist where user_id=?";
+            return jdbcTemplate.query(getWishListbyUserIDQuery,new WishListRowMapper(),userID);
 
         }catch (Exception e){
+            logger.error("Error Message: ");
             throw new RuntimeException(e.getMessage());
         }
     }
