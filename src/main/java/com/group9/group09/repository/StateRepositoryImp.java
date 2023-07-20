3812b0280605d74eca73c4186af4697ef26139fa
Riya <ry470536@dal.ca>
2023-07-20 17:00:21 -0300

    Logger added to StateRepositoryIMP

--- src/main/java/com/group9/group09/repository/StateRepositoryImp.java
+++ src/main/java/com/group9/group09/repository/StateRepositoryImp.java
@@ -3,6 +3,8 @@ package com.group9.group09.repository;
 import com.group9.group09.model.State;
 import com.group9.group09.repository.interfaces.StateRepository;
 import com.group9.group09.repository.rowmapper.StateRowMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -13,15 +15,18 @@ import java.util.Optional;
 public class StateRepositoryImp implements StateRepository {
     private final JdbcTemplate jdbcTemplate;
 
+    private static Logger logger = LoggerFactory.getLogger(StateRepositoryImp.class);
     public StateRepositoryImp(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
     public Optional<State> findByStateId(Integer stateId) {
         try {
+            logger.info("Info Message: ");
             String findStatebyIDQuery = "SELECT * FROM States where state_id = ?";
             return Optional.ofNullable(jdbcTemplate.queryForObject(findStatebyIDQuery, new StateRowMapper(), stateId));
         } catch (Exception e) {
+            logger.error("Error Message: ");
             System.out.println(e.getMessage());
             throw new RuntimeException();
         }
@@ -31,9 +36,11 @@ public class StateRepositoryImp implements StateRepository {
     public Optional<State> findByStateName(String stateName) {
 
         try {
+            logger.info("Info Message: ");
             String findStatebyNameQuery = "SELECT * FROM States where state = ?";
             return Optional.ofNullable(jdbcTemplate.queryForObject(findStatebyNameQuery, new StateRowMapper(), stateName));
         } catch (Exception e) {
+            logger.error("Error Message: ");
             System.out.println(e.getMessage());
             throw new RuntimeException();
         }
@@ -43,9 +50,11 @@ public class StateRepositoryImp implements StateRepository {
     public List<State> getStatesbyCountryID(Integer countryID) {
 
         try {
+            logger.info("Info Message: ");
             String getStatesbyCountryIDQuery = "SELECT * FROM States where country_id = ?";
             return jdbcTemplate.query(getStatesbyCountryIDQuery, new StateRowMapper(), countryID);
         } catch (Exception e) {
+            logger.error("Error Message: ");
             System.out.println(e.getMessage());
             throw new RuntimeException();
         }
