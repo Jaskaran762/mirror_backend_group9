8e74ab48f3a8b047b38e32fd6c8d2feb04a436e7
Riya <ry470536@dal.ca>
2023-07-20 16:05:20 -0300

    Logger added to UserController

--- src/main/java/com/group9/group09/controller/UserController.java
+++ src/main/java/com/group9/group09/controller/UserController.java
@@ -5,6 +5,8 @@ import com.group9.group09.DTO.ResponseDTO;
 import com.group9.group09.exception.UserNotFoundException;
 import com.group9.group09.model.User;
 import com.group9.group09.service.interfaces.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +24,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private static Logger logger = LoggerFactory.getLogger(UserController.class);
+
     /**
      * Handles the user login request.
      *
@@ -31,9 +35,11 @@ public class UserController {
     @PostMapping(path = "/login")
     public ResponseEntity<?> userLogin(@RequestBody User user) {
         try {
+            logger.info("Info Message: ");
             ResponseDTO serviceResponse = userService.loginUserService(user);
             return new ResponseEntity<>(serviceResponse, HttpStatus.OK);
         } catch (Exception e) {
+            logger.error("Error Message: ");
             ErrorResponse response = new ErrorResponse();
             response.setMessage("Login Issue");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
@@ -51,14 +57,17 @@ public class UserController {
     public ResponseEntity<?> userRegister(@RequestBody User user) {
 
         try {
+            logger.info("Info Message: ");
             ResponseDTO serviceResponse = userService.registerUserService(user);
             return new ResponseEntity<>(serviceResponse, HttpStatus.OK);
         } catch (UserNotFoundException e) {
+            logger.error("Error Message: ");
             ErrorResponse response = new ErrorResponse();
             response.setMessage(e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(response);
         } catch (Exception e) {
+            logger.error("Error Message: ");
             ErrorResponse response = new ErrorResponse();
             response.setMessage("Registration Issue");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
