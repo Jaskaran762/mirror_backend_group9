6fc603a8b452533a61ed45b8042ce062294c08f8
Aharnish Maheshbhai Solanki <ah910744@dal.ca>
2023-07-23 22:10:08 -0300

    Updated activity request and response DTO files, updated according to the table structure in the db and package structure

--- Backend/src/main/java/com/group9/group09/DTO/RequestDTO/ActivityRequestDTO.java
+++ Backend/src/main/java/com/group9/group09/DTO/RequestDTO/ActivityRequestDTO.java
@@ -1,4 +1,4 @@
-package com.group9.group09.DTO;
+package com.group9.group09.DTO.RequestDTO;
 
 /**
  * Data Transfer Object for Activity request.
@@ -7,6 +7,10 @@ public class ActivityRequestDTO extends RequestDTO {
 
     private Integer activityID;
     private String activityName;
+    private String description;
+    private Integer city_id;
+    private String city_name;
+    private String interest;
 
     /**
      * Get the activity ID.
@@ -42,5 +46,37 @@ public class ActivityRequestDTO extends RequestDTO {
      */
     public void setActivityName(String activityName) {
         this.activityName = activityName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getCity_id() {
+        return city_id;
+    }
+
+    public void setCity_id(Integer city_id) {
+        this.city_id = city_id;
+    }
+
+    public String getInterest() {
+        return interest;
+    }
+
+    public void setInterest(String interest) {
+        this.interest = interest;
+    }
+
+    public String getCity_name() {
+        return city_name;
+    }
+
+    public void setCity_name(String city_name) {
+        this.city_name = city_name;
     }
 }
