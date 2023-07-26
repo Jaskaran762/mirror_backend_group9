2e0c8b2a79e5fbc1aaf39154309dbd69b42a9f05
Aharnish Maheshbhai Solanki <ah910744@dal.ca>
2023-07-26 03:58:13 -0300

    DTOs updated (itinerary and wishlist)

--- Backend/src/main/java/com/group9/group09/DTO/RequestDTO/ItineraryRequestDTO.java
+++ Backend/src/main/java/com/group9/group09/DTO/RequestDTO/ItineraryRequestDTO.java
@@ -1,11 +1,18 @@
 package com.group9.group09.DTO.RequestDTO;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  * Data Transfer Object for Itinerary Request.
  */
 public class ItineraryRequestDTO extends RequestDTO{
 
     private Integer userid;
+    private Integer placeid;
+    private Integer activityid;
+    private String startdate;
+    private String enddate;
 
     /**
      * Get the user ID from the itinerary request.
@@ -23,5 +30,38 @@ public class ItineraryRequestDTO extends RequestDTO{
      */
     public void setUserid(Integer userid) {
         this.userid = userid;
+    }
+
+
+    public String getStartdate() {
+        return startdate;
+    }
+
+    public void setStartdate(String startdate) {
+        this.startdate = startdate;
+    }
+
+    public String getEnddate() {
+        return enddate;
+    }
+
+    public void setEnddate(String enddate) {
+        this.enddate = enddate;
+    }
+
+    public Integer getPlaceid() {
+        return placeid;
+    }
+
+    public void setPlaceid(Integer placeid) {
+        this.placeid = placeid;
+    }
+
+    public Integer getActivityid() {
+        return activityid;
+    }
+
+    public void setActivityid(Integer activityid) {
+        this.activityid = activityid;
     }
 }
