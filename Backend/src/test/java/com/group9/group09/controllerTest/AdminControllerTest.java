b88d9de5607f1b4dc03bf0925405fb3a587c7d2a
boza <bh327148@dal.ca>
2023-07-26 14:55:13 -0300

    controller test cases added

--- Backend/src/test/java/com/group9/group09/controllerTest/AdminControllerTest.java
+++ Backend/src/test/java/com/group9/group09/controllerTest/AdminControllerTest.java
@@ -1,223 +1,86 @@
 package com.group9.group09.controllerTest;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 import com.group9.group09.DTO.RequestDTO.CityRequestDTO;
-import com.group9.group09.DTO.RequestDTO.CountryRequestDTO;
-import com.group9.group09.DTO.RequestDTO.PlaceRequestDTO;
-import com.group9.group09.DTO.RequestDTO.StateRequestDTO;
-import com.group9.group09.DTO.ResponseDTO.ErrorResponse;
 import com.group9.group09.DTO.ResponseDTO.ResponseDTO;
 import com.group9.group09.controller.AdminController;
 import com.group9.group09.service.interfaces.AdminService;
+import com.group9.group09.config.JwtService;
+import com.group9.group09.exception.NotAdminAccessException;
+import com.group9.group09.model.User;
+import com.group9.group09.repository.interfaces.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@RunWith(MockitoJUnitRunner.class)
-public class AdminControllerTest {/*
+class AdminControllerTest {
 
     @Mock
-    private AdminService adminServiceMock;
+    private AdminService adminService;
+
+    @Mock
+    private JwtService jwtService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private HttpServletRequest httpServletRequest;
 
     @InjectMocks
     private AdminController adminController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
-    public void testAddCountry_Success() {
-        // Mock AdminService
-        AdminService adminServiceMock = Mockito.mock(AdminService.class);
-        CountryRequestDTO countryRequest = new CountryRequestDTO();
-        HttpServletRequest request = new MockHttpServletRequest();
+    void testAddCity_Success() {
+        CityRequestDTO cityRequestDTO = new CityRequestDTO();
+        when(httpServletRequest.getHeader("Authorization")).thenReturn("Bearer test-token");
 
-        // Set up the mock behavior for adminService.addCountryService
-        ResponseDTO successResponse = new ResponseDTO();
-        Mockito.when(adminServiceMock.addCountryService(countryRequest)).thenReturn(successResponse);
+        // Mock jwtService's extractUsername method
+        when(jwtService.extractUsername("test-token")).thenReturn("test-username");
 
-        // Set up controller
-        AdminController adminController = new AdminController();
-       // adminController.setAdminService(adminServiceMock);
+        // Mock userRepository's findByUsermail method
+        User user = new User();
+        user.setUserId("1");
+        user.setIsAdmin(1); // Set user as admin
+        Optional<User> optionalUser = Optional.of(user);
+        when(userRepository.findByUsermail("test-username")).thenReturn(optionalUser);
 
-        // Execute the method
-        ResponseEntity<?> responseEntity = adminController.addCountry(countryRequest, request);
+        // Mock adminService's addCityService method
+        ResponseDTO mockResponse = new ResponseDTO();
+        when(adminService.addCityService(any(CityRequestDTO.class))).thenReturn(mockResponse);
 
-        // Assert the response
+        ResponseEntity<?> responseEntity = adminController.addState(cityRequestDTO, httpServletRequest);
+
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(successResponse, responseEntity.getBody());
+        assertEquals(mockResponse, responseEntity.getBody());
     }
 
-    @Test
-    public void testAddCountry_Exception() {
-        // Mock AdminService
-        AdminService adminServiceMock = Mockito.mock(AdminService.class);
-        CountryRequestDTO countryRequest = new CountryRequestDTO();
-        HttpServletRequest request = new MockHttpServletRequest();
+    // Add more test cases for other controller methods...
 
-        // Set up the mock behavior to throw an exception
-        Mockito.when(adminServiceMock.addCountryService(countryRequest)).thenThrow(new RuntimeException());
-
-        // Set up controller
-        AdminController adminController = new AdminController();
-        //adminController.setAdminService(adminServiceMock);
-
-        // Execute the method
-        ResponseEntity<?> responseEntity = adminController.addCountry(countryRequest, request);
-
-        // Assert the response
-        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
-        assertTrue(responseEntity.getBody() instanceof ErrorResponse);
-        ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
-        assertEquals("Add country admin api failed", errorResponse.getMessage());
-    }
-
-    @Test
-    public void testAddState_Success() {
-        // Mock AdminService
-        AdminService adminServiceMock = Mockito.mock(AdminService.class);
-        StateRequestDTO stateRequest = new StateRequestDTO();
-        HttpServletRequest request = new MockHttpServletRequest();
-
-        // Set up the mock behavior for adminService.addStateService
-        ResponseDTO successResponse = new ResponseDTO();
-        Mockito.when(adminServiceMock.addStateService(stateRequest)).thenReturn(successResponse);
-
-        // Set up controller
-        AdminController adminController = new AdminController();
-        //adminController.setAdminService(adminServiceMock);
-
-        // Execute the method
-        ResponseEntity<?> responseEntity = adminController.addState(stateRequest, request);
-
-        // Assert the response
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(successResponse, responseEntity.getBody());
-    }
-
-    @Test
-    public void testAddState_Exception() {
-        // Mock AdminService
-        AdminService adminServiceMock = Mockito.mock(AdminService.class);
-        StateRequestDTO stateRequest = new StateRequestDTO();
-        HttpServletRequest request = new MockHttpServletRequest();
-
-        // Set up the mock behavior to throw an exception
-        Mockito.when(adminServiceMock.addStateService(stateRequest)).thenThrow(new RuntimeException());
-
-        // Set up controller
-        AdminController adminController = new AdminController();
-        //adminController.setAdminService(adminServiceMock);
-
-        // Execute the method
-        ResponseEntity<?> responseEntity = adminController.addState(stateRequest, request);
-
-        // Assert the response
-        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
-        assertTrue(responseEntity.getBody() instanceof ErrorResponse);
-        ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
-        assertEquals("Add state admin api failed", errorResponse.getMessage());
-    }
-
-    @Test
-    public void testAddCity_Success() {
-        // Mock AdminService
-        AdminService adminServiceMock = Mockito.mock(AdminService.class);
-        CityRequestDTO cityRequest = new CityRequestDTO();
-        HttpServletRequest request = new MockHttpServletRequest();
-
-        // Set up the mock behavior for adminService.addCityService
-        ResponseDTO successResponse = new ResponseDTO();
-        Mockito.when(adminServiceMock.addCityService(cityRequest)).thenReturn(successResponse);
-
-        // Set up controller
-        AdminController adminController = new AdminController();
-        //adminController.setAdminService(adminServiceMock);
-
-        // Execute the method
-       // ResponseEntity<?> responseEntity = adminController.addCity(cityRequest, request);
-
-        // Assert the response
-        //assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        //assertEquals(successResponse, responseEntity.getBody());
-    }
-
-    @Test
-    public void testAddCity_Exception() {
-        // Mock AdminService
-        AdminService adminServiceMock = Mockito.mock(AdminService.class);
-        CityRequestDTO cityRequest = new CityRequestDTO();
-        HttpServletRequest request = new MockHttpServletRequest();
-
-        // Set up the mock behavior to throw an exception
-        Mockito.when(adminServiceMock.addCityService(cityRequest)).thenThrow(new RuntimeException());
-
-        // Set up controller
-        AdminController adminController = new AdminController();
-        //adminController.setAdminService(adminServiceMock);
-
-        // Execute the method
-      //  ResponseEntity<?> responseEntity = adminController.addCity(cityRequest, request);
-
-        // Assert the response
-//        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
-//        assertTrue(responseEntity.getBody() instanceof ErrorResponse);
-//        ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
-//        assertEquals("Add city admin api failed", errorResponse.getMessage());
-    }
-
-    @Test
-    public void testAddPlace_Success() {
-        // Mock AdminService
-        AdminService adminServiceMock = Mockito.mock(AdminService.class);
-        PlaceRequestDTO placeRequest = new PlaceRequestDTO();
-        HttpServletRequest request = new MockHttpServletRequest();
-
-        // Set up the mock behavior for adminService.addPlaceService
-        ResponseDTO successResponse = new ResponseDTO();
-        Mockito.when(adminServiceMock.addPlaceService(placeRequest)).thenReturn(successResponse);
-
-        // Set up controller
-        AdminController adminController = new AdminController();
-       // adminController.setAdminService(adminServiceMock);
-
-        // Execute the method
-        ResponseEntity<?> responseEntity = adminController.addPlace(placeRequest, request);
-
-        // Assert the response
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(successResponse, responseEntity.getBody());
-    }
-
-    @Test
-    public void testAddPlace_Exception() {
-        // Mock AdminService
-        AdminService adminServiceMock = Mockito.mock(AdminService.class);
-        PlaceRequestDTO placeRequest = new PlaceRequestDTO();
-        HttpServletRequest request = new MockHttpServletRequest();
-
-        // Set up the mock behavior to throw an exception
-        Mockito.when(adminServiceMock.addPlaceService(placeRequest)).thenThrow(new RuntimeException());
-
-        // Set up controller
-        AdminController adminController = new AdminController();
-        //adminController.setAdminService(adminServiceMock);
-
-        // Execute the method
-        ResponseEntity<?> responseEntity = adminController.addPlace(placeRequest, request);
-
-        // Assert the response
-        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
-        assertTrue(responseEntity.getBody() instanceof ErrorResponse);
-        ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
-        assertEquals("Add place admin api failed", errorResponse.getMessage());
-    }
-*/
+    // For instance, add test cases for:
+    // - testAddCountry_Success
+    // - testAddCountry_NotAdminAccessException
+    // - testAddState_Success
+    // - testAddState_NotAdminAccessException
+    // - testAddPlace_Success
+    // - testAddPlace_NotAdminAccessException
+    // - testAddActivity_Success
+    // - testAddActivity_NotAdminAccessException
 }
