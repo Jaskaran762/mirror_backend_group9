f786fa2ec4ab80931ba3cdefb4f66a64836272d6
Aharnish Maheshbhai Solanki <ah910744@dal.ca>
2023-07-26 17:54:56 -0300

    reviews place request  and response dto updated

--- Backend/src/main/java/com/group9/group09/DTO/RequestDTO/ReviewsPlaceRequestDTO.java
+++ Backend/src/main/java/com/group9/group09/DTO/RequestDTO/ReviewsPlaceRequestDTO.java
@@ -1,24 +1,41 @@
 package com.group9.group09.DTO.RequestDTO;
 
 public class ReviewsPlaceRequestDTO extends RequestDTO{
-    /*
-    private Integer userid;
+
+    private Integer user_id;
+    private Integer rating;
+    private Integer place_id;
+    private String review_message;
+
+    public Integer getPlace_id() {
+        return place_id;
+    }
+
+    public void setPlace_id(Integer place_id) {
+        this.place_id = place_id;
+    }
 
     public Integer getUserid() {
-        return userid;
+        return user_id;
     }
 
     public void setUserid(Integer userid) {
-        this.userid = userid;
-    }
-*/
-    private Integer placeid;
-
-    public Integer getPlaceid() {
-        return placeid;
+        this.user_id = userid;
     }
 
-    public void setPlaceid(Integer placeid) {
-        this.placeid = placeid;
+    public String getReview_message() {
+        return review_message;
+    }
+
+    public void setReview_message(String review_message) {
+        this.review_message = review_message;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
     }
 }
