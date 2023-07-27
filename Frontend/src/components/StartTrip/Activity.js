7266cd26823c1e69b25d87a2dc1d3103fceeec1f
Abhisha Ashikbhai Thaker <ab570421@dal.ca>
2023-07-27 18:17:30 -0300

    conditionally displayed reviewDetail when response is available and beautified it a bit

--- Frontend/src/components/StartTrip/Activity.js
+++ Frontend/src/components/StartTrip/Activity.js
@@ -1,22 +1,40 @@
 import React, { useState, useEffect }  from 'react';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios  from 'axios';
 import HomeNavbar from '../HomeNav';
 import Footer from '../footer';
 import { Container, Row, Col, Button, Card, Modal, Form } from 'react-bootstrap';
 
 const Activity = () => {
+   const changePage = useNavigate();
    const { activityid }  = useParams();
    const [activityDetail,setactivityDetail] = useState();
-
    const [reviewDetail, setreviewDetail ] = useState([]);
    const token = sessionStorage.getItem('token');
    const headers = {
    Authorization: `Bearer ${token}`,
    };
-   console.log(headers);
+
    const activityidnum = parseInt(activityid, 10);
-    console.log(activityidnum);
+
+       // change numerical rating to stars
+       const renderStars = (rating) => {
+        const stars = [];
+          for (let i = 1; i <= 5; i++) {
+            stars.push(
+              <span key={i} style={{ color: i <= rating ? 'gold' : 'gray' }}>
+                    ★
+              </span>
+          );
+          }
+          return stars;
+        };
+  // Add Review button on click
+  const handleAddReview = () => {
+    changePage(`/addReview/${activityid}`);
+  };
+
     useEffect(()=>{
         axios.post('http://localhost:8090/home/activities', { activityID:activityidnum }, { headers })
         .then((response) => {
@@ -25,18 +43,19 @@ const Activity = () => {
             console.log(activityDetail);
           })
           .catch((error) => {
-            console.error('Error fetching place regions:', error);
+            console.error('Error fetching activity regions:', error);
           });
     
         },[])
+
           useEffect(()=>{
-            axios.post('http://localhost:8090/home/reviewactivity', { activityid:activityidnum }, { headers })
+            axios.post('http://localhost:8090/home/reviewactivity', { activityid: activityidnum }, { headers })
             .then((response) => {
                 console.log(response.data.reviewsActivities);
                 setreviewDetail(response.data.reviewsActivities);
               })
               .catch((error) => {
-                console.error('Error fetching places list:', error);
+                console.error('Error fetching review activity list:', error);
               });
         
             },[])
@@ -54,42 +73,43 @@ const Activity = () => {
               if (!reviewDetail) {
                 return <div>Loading..reviews</div>;
               }
-              const renderStars = (rating) => {
-                const stars = [];
-                for (let i = 1; i <= 5; i++) {
-                  stars.push(
-                    <span key={i} style={{ color: i <= rating ? 'gold' : 'gray' }}>
-                      ★
-                    </span>
-                  );
-                }
-                return stars;
-              };
+
    // Now you can use the fetched placeList and activityList data here
     return (
       <div>
-      <div style={{ marginTop: '0px' }}>
-        <HomeNavbar />
-      </div>
-     
-      <Container>
+      <HomeNavbar />     
+      <Container style={{ marginTop: '2%'}}>
         <Row> 
-           <h1>  { activityDetail.activityName }</h1>
+        <Col>
+            {activityDetail && (
+              <>
+               <h1> {activityDetail.activityName}</h1>
+               <img src={activityDetail.activityLink} alt={activityDetail.activityName} style={{ width:'65%'}}></img>
+                <br/>
+                <p style={{ marginTop: '2%'}}> {activityDetail.activitydesc} </p>
+              </>
+            )} 
+          </Col>
         </Row>
-     <p> { activityDetail.activitydesc } </p>
-      <img src={ activityDetail.activityLink }></img> 
+      <div style={{borderTop:'1px solid #ccc'}}></div>
+      <br/>
+      { reviewDetail.length > 0 && (
       <Row>
-        <h2>Reviews</h2>
+        <h2> Activity Reviews</h2>
        </Row>
+      )}
+      <br />
       {reviewDetail.map((review) => (
-                       <div key={review.reviewActivityID}>
-                       <p>Rating: {renderStars(review.rating)}</p>
-                       <p>Comment: {review.reviewactivityComment}</p>
-                       <p> DateofReview : { review.dateofreview } </p>
+          <div key={review.reviewActivityID}>
+              <p>Rating: {renderStars(review.rating)}</p>
+               <p>Review: {review.reviewactivityComment}</p>
+                       {/* <p> DateofReview : { review.dateofreview } </p> */}
                        {/* Render other review details as needed */}
-                     </div>
-                      ))}
-                      <div style={{ marginTop: '10rem' }}>
+          </div>
+      ))}
+      <br/>
+      <Button onClick={handleAddReview}>Add review</Button>
+      <div style={{ marginTop: '10rem' }}>
         <Footer />
       </div>
       </Container>
