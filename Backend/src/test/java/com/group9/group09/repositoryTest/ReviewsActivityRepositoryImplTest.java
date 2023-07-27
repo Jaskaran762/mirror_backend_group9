ae637b58578b4dfb511b6e9744003294df1c988c
boza <bh327148@dal.ca>
2023-07-27 02:03:23 -0300

    ReviewsActivityRepositoryImpl Testcases

--- Backend/src/test/java/com/group9/group09/repositoryTest/ReviewsActivityRepositoryImplTest.java
+++ Backend/src/test/java/com/group9/group09/repositoryTest/ReviewsActivityRepositoryImplTest.java
@@ -1,4 +1,56 @@
 package com.group9.group09.repositoryTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import com.group9.group09.model.ReviewsActivity;
+import com.group9.group09.repository.ReviewsActivityRepositoryImpl;
+import com.group9.group09.repository.rowmapper.ReviewsActivityRowMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
 public class ReviewsActivityRepositoryImplTest {
+
+    @Mock
+    private JdbcTemplate jdbcTemplate;
+
+    @InjectMocks
+    private ReviewsActivityRepositoryImpl reviewsActivityRepository;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testGetReviewsActivitybyActivityId() {
+        int activityId = 1;
+        List<ReviewsActivity> expectedReviews = List.of(new ReviewsActivity(), new ReviewsActivity());
+
+        // Mocking the behavior of the jdbcTemplate.query method
+        when(jdbcTemplate.query(anyString(), any(ReviewsActivityRowMapper.class), eq(activityId)))
+                .thenReturn(expectedReviews);
+
+        List<ReviewsActivity> result = reviewsActivityRepository.getReviewsActivitybyActivityId(activityId);
+
+        assertEquals(expectedReviews, result);
+    }
+
+    @Test
+    public void testGetReviewsActivitybyActivityIdError() {
+        int activityId = 1;
+
+        // Mocking the behavior of the jdbcTemplate.query method to throw an exception
+        when(jdbcTemplate.query(anyString(), any(ReviewsActivityRowMapper.class), eq(activityId)))
+                .thenThrow(new RuntimeException("Test Exception"));
+
+        // Ensure that the method under test handles the exception and rethrows it as a RuntimeException
+        assertThrows(RuntimeException.class, () -> reviewsActivityRepository.getReviewsActivitybyActivityId(activityId));
+    }
 }
