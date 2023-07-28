d0bb5897e9c32b8323258d44bd20cb537176d743
Aharnish Maheshbhai Solanki <ah910744@dal.ca>
2023-07-28 02:25:45 -0300

    counttry list updated

--- Frontend/src/components/authentication/Signup.js
+++ Frontend/src/components/authentication/Signup.js
@@ -1,52 +1,53 @@
-import React, { useState } from 'react';
-import { Container, Row, Col, Form, Button, Toast } from 'react-bootstrap';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Container, Row, Col, Form, Button, Toast } from "react-bootstrap";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [contact_no, setPhone] = useState('');
-  const [homeCountry, setHomeCountry] = useState('Canada');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [contact_no, setPhone] = useState("");
+  const [homeCountry, setHomeCountry] = useState("Canada");
   const [isLoading, setIsLoading] = useState(false);
-  const [interest, setInterests] = useState('');
-  const [showSuccessToast, setShowSuccessToast] = useState(false); 
-  const [showErrorToast, setShowErrorToast] = useState(false); 
+  const [interest, setInterests] = useState("");
+  const [showSuccessToast, setShowSuccessToast] = useState(false);
+  const [showErrorToast, setShowErrorToast] = useState(false);
 
-  const interestsOptions = [ 
-    'Adventure',
-    'Beach',
-    'City Exploration',
-    'Nature',
-    'Food',
-    'History',
+  const interestsOptions = [
+    "Adventure",
+    "Beach",
+    "City Exploration",
+    "Nature",
+    "Food",
+    "History",
   ];
 
   const handleChange = (e) => {
     const { name, value, checked } = e.target;
-    if (name === 'interest') {
-      
+    if (name === "interest") {
       if (checked) {
         setInterests((prevInterests) => [...prevInterests, value]);
       } else {
-        setInterests((prevInterests) => prevInterests.filter((interest) => interest !== value));
+        setInterests((prevInterests) =>
+          prevInterests.filter((interest) => interest !== value)
+        );
       }
     } else {
       switch (name) {
-        case 'name':
+        case "name":
           setName(value);
           break;
-        case 'email':
+        case "email":
           setEmail(value);
           break;
-        case 'password':
+        case "password":
           setPassword(value);
           break;
-        case 'contact_no':
+        case "contact_no":
           setPhone(value);
           break;
-        case 'homeCountry':
+        case "homeCountry":
           setHomeCountry(value);
           break;
         default:
@@ -55,20 +56,23 @@ const Signup = () => {
     }
   };
 
-
-  const changePage=useNavigate();
+  const changePage = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const intrestString = interest.join(',');
+    const intrestString = interest.join(",");
     console.log(intrestString);
     setIsLoading(true);
     const homeCountryValueMap = {
-      Canada: 1,
-      USA: 2,
-      India: 3,
-      London: 4,
-      France: 5,
-      Paris: 6,
+      India: 1,
+      Canada: 2,
+      UnitedStates: 3,
+      UnitedKingdom: 4,
+      Australia: 5,
+      China: 6,
+      France: 7,
+      Germany: 8,
+      Italy: 9,
+      NewZealand: 10,
     };
 
     const userObject = {
@@ -77,24 +81,25 @@ const Signup = () => {
       name,
       email,
       contact_no,
-      homeCountry: homeCountryValueMap[homeCountry], 
+      homeCountry: homeCountryValueMap[homeCountry],
     };
 
-
     try {
-      const response = await axios.post('http://localhost:8090/auth/register', userObject);
-      console.log('API response:', response.data);
-      sessionStorage.setItem("token",response?.data?.token ?? "");
-      setIsLoading(false); 
-      setShowSuccessToast(true); 
+      const response = await axios.post(
+        "http://localhost:8090/auth/register",
+        userObject
+      );
+      console.log("API response:", response.data);
+      sessionStorage.setItem("token", response?.data?.token ?? "");
+      setIsLoading(false);
+      setShowSuccessToast(true);
       setTimeout(() => {
         setShowSuccessToast(false);
-        changePage('/landing');
+        changePage("/landing");
       }, 2000);
-    } 
-    catch (error) {
-      setIsLoading(false); 
-      console.error('API error:', error);
+    } catch (error) {
+      setIsLoading(false);
+      console.error("API error:", error);
       setShowErrorToast(true);
       setTimeout(() => {
         setShowErrorToast(false);
@@ -107,33 +112,60 @@ const Signup = () => {
       className="registerPage"
       style={{
         backgroundImage: `url('https://img.freepik.com/free-photo/top-view-passport-tickets-camera_23-2148786120.jpg?w=1060&t=st=1687293728~exp=1687294328~hmac=0237669a31b51fa53dc2b368ce4a4b1c6695c7f9f20bed1cab6f09b88d09f5c4')`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'right',
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "right",
       }}
     >
       <Container className="vh-100">
         <Row className="justify-content-left align-items-center h-100">
           <Col xs={10} md={3} lg={5}>
-            <div className="signupContainer p-4 rounded-lg shadow" style={{ backgroundColor: '#ffffff' }}>
+            <div
+              className="signupContainer p-4 rounded-lg shadow"
+              style={{ backgroundColor: "#ffffff" }}
+            >
               <h3 className="title">Want to plan your trip?</h3>
               <h4 className="title">Sign up with Tripify</h4>
               <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="formName">
                   <Form.Label>Name</Form.Label>
-                  <Form.Control type="text" name="name" placeholder="Enter your name" value={name} onChange={handleChange} />
+                  <Form.Control
+                    type="text"
+                    name="name"
+                    placeholder="Enter your name"
+                    value={name}
+                    onChange={handleChange}
+                  />
                 </Form.Group>
                 <Form.Group controlId="formEmail">
                   <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" name="email" placeholder="Enter your email" value={email} onChange={handleChange} />
+                  <Form.Control
+                    type="email"
+                    name="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={handleChange}
+                  />
                 </Form.Group>
                 <Form.Group controlId="formPassword">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" name="password" placeholder="Choose a password" value={password} onChange={handleChange} />
+                  <Form.Control
+                    type="password"
+                    name="password"
+                    placeholder="Choose a password"
+                    value={password}
+                    onChange={handleChange}
+                  />
                 </Form.Group>
                 <Form.Group controlId="formPhone">
                   <Form.Label>Phone</Form.Label>
-                  <Form.Control type="text" name="contact_no" placeholder="Enter your phone number" value={contact_no} onChange={handleChange} />
+                  <Form.Control
+                    type="text"
+                    name="contact_no"
+                    placeholder="Enter your phone number"
+                    value={contact_no}
+                    onChange={handleChange}
+                  />
                 </Form.Group>
                 <Form.Group controlId="formInterest">
                   <Form.Label>Your Interests</Form.Label>
@@ -144,7 +176,7 @@ const Signup = () => {
                       name="interest"
                       value={interestOption}
                       label={interestOption}
-                      checked={interest.includes(interestOption)} 
+                      checked={interest.includes(interestOption)}
                       onChange={handleChange}
                     />
                   ))}
@@ -152,18 +184,30 @@ const Signup = () => {
                 <br />
                 <Form.Group controlId="formCountrySelect">
                   <Form.Label>Country</Form.Label>
-                  <Form.Control as="select" name="homeCountry" value={homeCountry} onChange={handleChange}>
+                  <Form.Control
+                    as="select"
+                    name="homeCountry"
+                    value={homeCountry}
+                    onChange={handleChange}
+                  >
                     <option value="Canada">Canada</option>
-                    <option value="USA">USA</option>
-                    <option value="India">India</option>
-                    <option value="London">London</option>
+                    <option value="United States">United States</option>
+                    <option value="United Kingdom">United Kingdom</option>
+                    <option value="Australia">Australia</option>
+                    <option value="China">China</option>
                     <option value="France">France</option>
-                    <option value="Paris">Paris</option>
+                    <option value="Germany">Germany</option>
+                    <option value="Italy">Italy</option>
+                    <option value="New Zealand">New Zealand</option>
                   </Form.Control>
                 </Form.Group>
                 <br />
-                <Button variant="primary" className="submitButton" type="submit">
-                  {isLoading ? 'Loading...' : 'Register'}
+                <Button
+                  variant="primary"
+                  className="submitButton"
+                  type="submit"
+                >
+                  {isLoading ? "Loading..." : "Register"}
                 </Button>
               </Form>
             </div>
@@ -174,14 +218,14 @@ const Signup = () => {
         show={showSuccessToast}
         onClose={() => setShowSuccessToast(false)}
         style={{
-          position: 'absolute',
-          top: '20px',
-          right: '20px',
+          position: "absolute",
+          top: "20px",
+          right: "20px",
           zIndex: 9999,
-          backgroundColor: '#28a745',
-          color: '#ffffff', 
+          backgroundColor: "#28a745",
+          color: "#ffffff",
         }}
-        delay={2000} 
+        delay={2000}
         autohide
       >
         <Toast.Body>Registration successful! Redirecting...</Toast.Body>
@@ -191,14 +235,14 @@ const Signup = () => {
         show={showErrorToast}
         onClose={() => setShowErrorToast(false)}
         style={{
-          position: 'absolute',
-          top: '20px',
-          right: '20px',
+          position: "absolute",
+          top: "20px",
+          right: "20px",
           zIndex: 9999,
-          backgroundColor: '#dc3545', 
-          color: '#ffffff', 
+          backgroundColor: "#dc3545",
+          color: "#ffffff",
         }}
-        delay={2000} 
+        delay={2000}
         autohide
       >
         <Toast.Body>Registration failed. Please try again.</Toast.Body>
