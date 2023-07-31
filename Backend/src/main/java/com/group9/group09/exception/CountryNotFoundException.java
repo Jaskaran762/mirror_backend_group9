a321cbe8c2d8d3c23e0d8072352182013e101abc
Riya <ry470536@dal.ca>
2023-07-31 20:47:26 -0300

    exceptions comments done

--- Backend/src/main/java/com/group9/group09/exception/CountryNotFoundException.java
+++ Backend/src/main/java/com/group9/group09/exception/CountryNotFoundException.java
@@ -1,10 +1,24 @@
 package com.group9.group09.exception;
 
-public class CountryNotFoundException extends RuntimeException{
+/**
+ * Custom exception class to indicate that a country is not found.
+ * Extends the RuntimeException class, which is an unchecked exception.
+ */
+public class CountryNotFoundException extends RuntimeException {
 
-    public CountryNotFoundException(){super();}
+    /**
+     * Constructs a new CountryNotFoundException with no specified error message.
+     */
+    public CountryNotFoundException() {
+        super();
+    }
 
-    public CountryNotFoundException(String message){
+    /**
+     * Constructs a new CountryNotFoundException with the specified error message.
+     *
+     * @param message The error message explaining the reason for the exception.
+     */
+    public CountryNotFoundException(String message) {
         super(message);
     }
 }
--- Backend/src/main/java/com/group9/group09/exception/UserNotFoundException.java
+++ Backend/src/main/java/com/group9/group09/exception/UserNotFoundException.java
@@ -1,12 +1,24 @@
 package com.group9.group09.exception;
 
-// Custom exception class for user not found.
+/**
+ * Custom exception class to indicate that a user is not found.
+ * Extends the RuntimeException class, which is an unchecked exception.
+ */
 public class UserNotFoundException extends RuntimeException {
 
-    // Constructs a new UserNotFoundException with no detail message.
+    /**
+     * Constructs a new UserNotFoundException with no specified error message.
+     */
     public UserNotFoundException() {
         super();
     }
 
-    public UserNotFoundException(String message){ super(message);}
+    /**
+     * Constructs a new UserNotFoundException with the specified error message.
+     *
+     * @param message The error message explaining the reason for the exception.
+     */
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
